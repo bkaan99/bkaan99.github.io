4349ec778b563bee17100afbfc2c5d5400ee06c1
Bilge Kaan <bilgekagangurgen@msn.com>
2022-01-25 17:20:49 +0300

    oldu bu

--- js/custom.js
+++ js/custom.js
@@ -104,8 +104,8 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
-
+    $("#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo01").animatedModal2();
     // Contact Form 	
 
     // validate contact form
